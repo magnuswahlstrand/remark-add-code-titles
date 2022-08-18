df27e6257ae4160c7604e69295791b90c8819cb3
Magnus <magnus@magnuss-mbp.lan>
2022-08-18 23:01:23 +0200

    make an ESM module

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,6 +2,7 @@ export const transform = () => {
     console.log("hej")
 }
 
-export const attacher = () => {
+export const attacher = (config = {}) => {
+    console.log(config)
     return transform;
 };
