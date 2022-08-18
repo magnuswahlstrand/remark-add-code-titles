ec695dba55f6b78805ea9cab24f31e722b675785
Magnus <magnus@magnuss-mbp.lan>
2022-08-18 23:52:02 +0200

    simple plugin working

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,40 @@
-export const transform = () => {
-    console.log("hej")
+import {visit} from 'unist-util-visit';
+import type {Root as MdastRoot} from "mdast";
+import type {Plugin} from "unified";
+
+// export const transformV1 = () => {
+//     console.log("hej")
+// }
+
+export interface IOptions {
+    marker: string;
 }
 
-export const attacher = (config = {}) => {
+export const transform = (marker: string) => {
+    return (tree: MdastRoot) => {
+        const visitor = (node: any) => {
+            const {children} = node;
+            console.log("childre", children, children.length, children[0].value, children[0].value === marker)
+            console.log(children[0].value)
+            console.log(marker)
+            if (children.length >= 1 && children[0].value === marker) {
+                console.log("matching", children, marker)
+                node.type = 'code';
+                node.children = undefined;
+                node.lang = 'js';
+                node.value = `const a = 1;`;
+            }
+        };
+        console.log('yay')
+        visit(tree, 'paragraph', visitor);
+    }
+}
+
+type PluginOptions = {
+    marker?: string;
+};
+
+export const attacher: Plugin<[PluginOptions], MdastRoot> = (config: PluginOptions = {}) => {
     console.log(config)
-    return transform;
+    return transform(config.marker ?? "");
 };
--- test-cmd/index.ts
+++ test-cmd/index.ts
@@ -1,17 +1,18 @@
 import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
 import {unified} from 'unified'
+import rehypeStringify from 'rehype-stringify'
 import {attacher} from '../src/index.mjs'
 
-const tree = unified()
+const tree = await unified()
     .use(remarkParse)
-    .use(attacher, {title:'hej'})
-    .parse('' +
-        '# Hello world!\n' +
+    .use(attacher, {marker:'Here is a paragraph'})
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process('# Hello world!\n' +
         'Here is a paragraph')
 
 // console.log(tree)
-console.log(tree)
+console.log(String(tree))
 console.log("")
 console.log("")
-
-attacher()();
